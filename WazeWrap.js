00de41976edc8cfd88fbca158bb0142730315f08
MapOMatic <mapomatic.waze@gmail.com>
2023-04-23 22:25:02 -0400

    Update WazeWrap.js

--- WazeWrap.js
+++ WazeWrap.js
@@ -19,7 +19,7 @@ var WazeWrap = {};
 (function() {
     'use strict';
     const MIN_VERSION = '2019.05.01.01';
-    const WW_URL = 'https://cdn.jsdelivr.net/gh/WazeDev/WazeWrap@latest/WazeWrapLib.js'; //'https://cdn.staticaly.com/gh/WazeDev/WazeWrap/master/WazeWrapLib.js?env=dev';
+    const WW_URL = 'https://github.com/mapomatic/WazeWrap-dev/raw/master/WazeWrapLib.js'; //'https://cdn.staticaly.com/gh/WazeDev/WazeWrap/master/WazeWrapLib.js?env=dev';
 
     async function init(){
         const sandboxed = typeof unsafeWindow !== 'undefined';
